c9520daa322ff8b323c046ef924fa3ff735bfc7c
mmaidel- <mmaidel-@student.42.fr>
2022-04-22 23:42:32 +0200

    corrige 00

--- Exercicios_C03/ex00/ft_strcmp.c
+++ Exercicios_C03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mmaidel- <mmaidel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 07:32:33 by mmaidel-          #+#    #+#             */
-/*   Updated: 2022/04/22 19:19:01 by mmaidel-         ###   ########.fr       */
+/*   Updated: 2022/04/22 21:30:55 by mmaidel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ int	ft_strcmp(char *s1, char *s2)
 	start = 0;
 	while (s1[start] == s2[start] && s1[start] != '\0' && s2[start != '\0'])
 		start++;
-	return (s1[start - s2[start]]);
+	return (s1[start] - s2[start]);
 }
 
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
-    char s0[] = "Quatroaaaaa";
-    char s1[] = "Dezenas";
+    char s0[] = "amassa";
+    char s1[] = "Amassa";
     
     printf("%d", ft_strcmp(s0, s1));
+	
+	printf("\n");
+	printf("%d", strcmp(s0, s1));
 }
